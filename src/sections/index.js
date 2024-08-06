6ef09b303ff95d7d2179baadd015d69aa84bd72f
unknown <okparakelvin65@gmail.com>
2024-08-06 20:13:37 +0100

    FS trim

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,19 +1,4 @@
-import CustomerReviews from "./CustomerReviews";
 import Footer from "./Footer";
 import PopularProducts from "./PopularProducts";
-import Hero from "./Hero.jsx";
-import Services from "./Services";
-import SpecialOffer from "./SpecialOffer.jsx";
-import Subscribe from "./Subscribe";
-import SuperQuality from "./SuperQuality";
 
-export {
-  CustomerReviews,
-  Footer,
-  PopularProducts,
-  Hero,
-  Services,
-  SpecialOffer,
-  Subscribe,
-  SuperQuality,
-};
+export { Footer, PopularProducts };
